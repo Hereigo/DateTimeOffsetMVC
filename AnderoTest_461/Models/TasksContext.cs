fab69a0e9bdf5e21662ed547520d25088b2264d9
Hereigo <Hereigo@online.ua>
2018-08-31 17:48:42 +0300

    disable migrations

--- AnderoTest_461/Models/TasksContext.cs
+++ AnderoTest_461/Models/TasksContext.cs
@@ -1,23 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
 
 namespace AnderoTest_461.Models
 {
-    public class TasksContext : DbContext
-    {
-        // You can add custom code to this file. Changes will not be overwritten.
-        // 
-        // If you want Entity Framework to drop and regenerate your database
-        // automatically whenever you change your model schema, please use data migrations.
-        // For more information refer to the documentation:
-        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
-    
-        public TasksContext() : base("name=TasksContext")
-        {
-        }
+	public class TasksContext : DbContext
+	{
+		// You can add custom code to this file. Changes will not be overwritten.
+		// 
+		// If you want Entity Framework to drop and regenerate your database
+		// automatically whenever you change your model schema, please use data migrations.
+		// For more information refer to the documentation:
+		// http://msdn.microsoft.com/en-us/data/jj591621.aspx
+
+		public TasksContext() : base("name=TasksContext")
+		{
+		}
 
 		public System.Data.Entity.DbSet<AnderoTest_461.Models.Task> Tasks { get; set; }
 	}
